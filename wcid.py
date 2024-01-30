1bd48aad9a5564e09493a067149566eb60d4ac6a
alwayswannafeed <xxxgodxxxdogxxx@gmail.com>
2024-01-30 11:26:35 +0200

    First file

---
+++ wcid.py
@@ -0,0 +1,10 @@
+def Privet():
+    return "What am I doing in MechMat???"
+def Poka():
+    return "2+2=integral"
+def diffr_vector(v1,v2):
+    n=len(v1)
+    V=[]
+    for i in range(n):
+        V[i]=v1[i]-v2[i]
+    return V
